8f2fd42d63b20cbd9c148c5153ffed73134caf97
Sahana Chaudhary <89292729+Sahana-Chaudhary@users.noreply.github.com>
2025-01-16 21:17:02 +0530

    Time: 699 ms (61.84%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,5 @@
+/* Write your PL/SQL query statement below */
+SELECT today.id FROM weather today
+LEFT JOIN weather yesterday
+ON today.recorddate-1 = yesterday.recorddate
+WHERE today.temperature > yesterday.temperature ;
