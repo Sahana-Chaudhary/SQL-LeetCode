e99e5c948ec1466084251266303901cc00d31bff
Sahana Chaudhary <89292729+Sahana-Chaudhary@users.noreply.github.com>
2025-02-13 10:24:46 +0530

    Time: 730 ms (82.06%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,5 +1,5 @@
 /* Write your PL/SQL query statement below */
-SELECT Person.firstName, Person.lastname, Address.city, Address.state
-FROM Person 
+SELECT Person.firstName, Person.lastName, Address.city , Address.state
+FROM Person
 LEFT JOIN Address
-ON Person.personID = Address.personID;
+ON Person.personId = Address.personId;
