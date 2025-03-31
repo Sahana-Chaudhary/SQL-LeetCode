5cfaf442de971f3082b85198de6af9b1cf4aeba5
Sahana Chaudhary <89292729+Sahana-Chaudhary@users.noreply.github.com>
2025-03-31 17:20:46 +0530

    Time: 456 ms (93.76%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,5 @@
-/* Write your PL/SQL query statement below */
-SELECT today.id FROM weather today
-LEFT JOIN weather yesterday
-ON today.recorddate-1 = yesterday.recorddate
-WHERE today.temperature > yesterday.temperature ;
+# Write your MySQL query statement below
+SELECT w1.id 
+FROM Weather w1
+JOIN Weather w2
+WHERE DATEDIFF(w1.recordDate,w2.recordDate)=1 AND w1.temperature> w2.temperature;
